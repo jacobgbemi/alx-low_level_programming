59fa6d7a0c11b52d39965dcbdeefda9587b931ed
Gbemi Jacob Adebayo <adebayogbemi90@gmail.com>
2022-04-07 14:57:52 +0000

    Task 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main - Function begins
+*
+*Return: Function ends
+*/
+
+int main(void)
+{
+	write(STDOUT_FILENO, "and that piece of art is useful
+		\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
