162a8f9b72ffbc45c117436b630f69d4bf5aea1e
Gbemi Jacob Adebayo <adebayogbemi90@gmail.com>
2022-04-08 13:06:41 +0000

    Inventing is a combination of brains and materials

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,22 +9,16 @@
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
-	int digit1, digit2;
+	int d;
 
-
-	for (digit1 = 0; digit1 < 9; digit1++)
+	for (d = 0; d < 100; d++)
 	{
-		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+		putchar((d / 10) + '0');
+		putchar((d % 10) + '0');
+		if (d != 99)
 		{
-			putchar((digit1 % 10) + '0');
-			putchar((digit2 % 10) + '0');
-
-			if (digit1 == 8 && digit2 == 9)
-				continue
-
 			putchar(',');
 			putchar(' ');
 		}
