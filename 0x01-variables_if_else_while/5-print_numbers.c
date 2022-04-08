f718e4e3c7ec6d8803f882784128459ad4637e67
Gbemi Jacob Adebayo <adebayogbemi90@gmail.com>
2022-04-08 10:30:00 +0000

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+/* Auth: Gbemi Jacob Adebayo
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Function to prints all single digit numbers 
+ * of base 10 starting from 0.
+ *
+ * Return: Always 0.
+ */
+ 
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+
+	printf("\n");
+
+	return (0);
+}
