dbd23c8e4d3c39938d517c941e6cd60892059c08
Gbemi Jacob Adebayo <adebayogbemi90@gmail.com>
2022-04-08 10:13:49 +0000

    And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+/*
+* Auth.:Gbemi Jacob Adebayo
+*/
+
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
+ int main(void)
+{
+	char letter;
+	
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
