c006467b746cda28c4034c7b0b5287a73a0b30af
Gbemi Jacob Adebayo <adebayogbemi90@gmail.com>
2022-04-08 10:19:22 +0000

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+/*
+* Auth: Gbemi Jacob Adebayo
+*/
+
+#include <stdio.h>
+
+/**
+ * main - Function to pints the alphabet in lowercase, and then in uppercase.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	for (letter = 'A'; letter <= 'Z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
