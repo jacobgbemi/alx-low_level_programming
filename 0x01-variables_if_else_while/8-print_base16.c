a8f0d7eed6e3fc7cf6511ca803f7e222fcaf3004
Gbemi Jacob Adebayo <adebayogbemi90@gmail.com>
2022-04-08 10:44:10 +0000

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+/* Auth: Gbemi Jacob Adebayo
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Function to print all the numbers of base 16 in lowercase.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int num;
+	char letter;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	
+	for (letter = 'a'; letter <= 'f'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
