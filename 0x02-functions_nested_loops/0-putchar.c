711c41793bb7432eb4aa49da400705032b26a2ec
Gbemi Jacob Adebayo <adebayogbemi90@gmail.com>
2022-04-12 16:16:27 +0000

    print _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -12,12 +12,24 @@
 
 int main(void)
 {
-	char word[9] = "_putchar";
-	int i;
-
-	for (i = 0; i < 9; i++)
-		_putchar(word[i]);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
 }
+
+
+
+
+
+
+
+
+
