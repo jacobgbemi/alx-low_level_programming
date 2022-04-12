09dbd51053e891d322edc4aaf6344569acc5b988
Gbemi Jacob Adebayo <adebayogbemi90@gmail.com>
2022-04-12 13:44:49 +0000

    print lowercase letters

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,15 @@
-/*
+i/*
 * Auth: Gbemi Jacob Adebayo
 */
 
 #include "main.h"
 
 /**
- * main - print_alphabet function prototype to print in lowercase,
- * followed by a new line
- *
- * Return: Always 0.
- */
+* main - print_alphabet function prototype to print in lowercase,
+* followed by a new line
+*
+* Return: Nothing (void)
+*/
 
 void print_alphabet(void)
 {
