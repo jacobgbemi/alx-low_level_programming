fc15404a324ea81dbf4d5a601a7f544a2464fba8
Gbemi Jacob Adebayo <adebayogbemi90@gmail.com>
2022-04-12 09:45:24 +0000

    write out _putchar

---
+++ 0x02-functions_nested_loops/myheaders.h
@@ -0,0 +1,23 @@
+#ifndef MYHEADERS_H
+#define MYHEADERS_H
+/*
+ * Auth: Gbemi Jacob Adebayo
+ * Desc: Header file containing declarations for all functions
+ *       used in this project.
+ */
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+
+#endif
