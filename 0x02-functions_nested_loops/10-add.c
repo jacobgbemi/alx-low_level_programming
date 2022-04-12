86bc6143237ec31e9117a20a1c79eca356d7fd87
Gbemi Jacob Adebayo <adebayogbemi90@gmail.com>
2022-04-12 15:25:01 +0000

    doing addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+/*
+ * Auth: Gbemi Jacob Adebayo
+ */
+
+#include "main.h"
+
+/**
+ * add - Adds two integers.
+ * @num1: The first integer to be added.
+ * @num2: The second integer to be added.
+ *
+ * Return: The result of the addition.
+ */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
