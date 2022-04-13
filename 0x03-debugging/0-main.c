32b00bfd1a8fad379adfac92b4d5e4db3712f8c0
Gbemi Jacob Adebayo <adebayogbemi90@gmail.com>
2022-04-13 15:27:25 +0000

    positive or negative number

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,11 +4,12 @@
 */
 
 /**
- * positive_or_negative - tests function that prints if number is
+ * main - tests function that prints if number is
  * positive or negative
+ * Return: Always 0.
  */
 
-void positive_or_negative(void)
+int main(void)
 {
 	int i;
 
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -6,6 +6,7 @@
 
 #include <stdio.h>
 
+int main(void);
 void positive_or_negative(int i);
 int largest_number(int, int, int);
 int convert_day(int month, int day);
