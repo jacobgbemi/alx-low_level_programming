2d332a726f56a1f3e79daa75d41b85f69ae182d6
Gbemi Jacob Adebayo <adebayogbemi90@gmail.com>
2022-04-13 15:43:30 +0000

    positive or negative number

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -6,8 +6,7 @@
 
 #include <stdio.h>
 
-int main(void);
-int positive_or_negative(void);
+int positive_or_negative(int i);
 int largest_number(int, int, int);
 int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
