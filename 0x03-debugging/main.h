f81cffba9cc1191a80aee2ef1397acd8ea63756d
Gbemi Jacob Adebayo <adebayogbemi90@gmail.com>
2022-04-13 09:50:27 +0000

    Which number is largest

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,8 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
+/*
+* Auth: Gbemi Jacob Adebayo
+*/
 
 #include <stdio.h>
 
 void positive_or_negative(int i);
+int largest_number(int, int, int);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
 
 #endif /* MAIN_H */
