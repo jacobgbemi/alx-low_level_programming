3ded440dea307f8acdcedea3ac99fa8e5054623c
Gbemi Jacob Adebayo <102582566+jacobgbemi@users.noreply.github.com>
2022-04-15 15:22:41 +0100

    print an integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -11,17 +11,16 @@
 
 void print_number(int n)
 {
+	unsigned int num = n;
 
-    if (n < 0) {
-        putchar('-');
-        n = -n;
-    }
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
 
-    if (n == 0)
-        putchar('0');
+	if ((num / 10) > 0)
+		print_number(num / 10);
 
-    if (n/10)
-        printnumber(n/10);
-
-    putchar(n%10 + '0');
+	_putchar((num % 10) + '0');
 }
