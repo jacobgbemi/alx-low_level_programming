34cb0419de309d98ee46a8b7a432454aed57f7a1
Gbemi Jacob Adebayo <adebayogbemi90@gmail.com>
2022-04-20 11:29:19 +0000

    copy from one pointer to another

---
+++ 0x05-pointers_arrays_strings/9-main.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/*
+* Auth: Gbemi Jacob Adebayo
+*/
+
+/**
+ * main - check the code for me.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char s1[98];
+	char *p;
+
+	p = _strcpy(s1, "First, solve the problem. Then, write the code\n");
+	printf("%s", s1);
+	printf("%s", p);
+	return (0);
+}
