7805143a2abcf0d51fccba24300c817056781f03
Gbemi Jacob Adebayo <adebayogbemi90@gmail.com>
2022-04-20 14:24:53 +0000

    copy string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -15,11 +15,23 @@
  * Return: pointer to dest
  */
 
+
 char *_strcpy(char *dest, char *src)
 {
-	/* char *aux = dest; */
+	int len, i;
 
-	while (*src)
-		*dest++ = *src++;
+	len = 0;
+
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
 	return (dest);
 }
