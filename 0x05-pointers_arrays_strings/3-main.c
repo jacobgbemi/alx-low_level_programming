bb1c7d503719156de3b03733af1eddf4d4a6c2e4
Gbemi Jacob Adebayo <adebayogbemi90@gmail.com>
2022-04-20 10:06:24 +0000

    print out strings

---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/*
+* Auth: Gbemi Jacob Adebayo
+*/
+
+/**
+ * main - check the code for me.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *str;
+
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	_puts(str);
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+/*
+* Auth: Gbemi Jacob Adebayo
+ */
+
+#include "main.h"
+
+/**
+ * _puts - Prints a string to stdout.
+ * @str: The string to be printed.
+ */
+
+void _puts(char *str)
+{
+	while (*str)
+		_putchar(*str++);
+
+	_putchar('\n');
+}
