2b7a5a91e717e12d6d5ee5f88b1190f590b5cdc1
Gbemi Jacob Adebayo <adebayogbemi90@gmail.com>
2022-04-20 10:14:19 +0000

    reverse strings

---
+++ 0x05-pointers_arrays_strings/4-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/*
+* Auth: Gbemi Jacob Adebayo
+*/
+
+/**
+ * main - check the code for me.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char *str;
+
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	print_rev(str);
+	return (0);
+}
