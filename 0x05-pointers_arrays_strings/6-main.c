b7f1c79a32f6ceac8cd86357b3bd28a662fed41d
Gbemi Jacob Adebayo <adebayogbemi90@gmail.com>
2022-04-20 11:01:27 +0000

    print characters

---
+++ 0x05-pointers_arrays_strings/6-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/*
+* Auth: Gbemi Jacob Adebayo
+*/
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char *str;
+
+	str = "0123456789";
+	puts2(str);
+	return (0);
+}
