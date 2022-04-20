e950aebd4c280d41e641f8be8146d7881994a8a8
Gbemi Jacob Adebayo <adebayogbemi90@gmail.com>
2022-04-20 11:39:32 +0000

    copy from one pointer to another

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -14,15 +14,11 @@
  * Return: pointer to dest
  */
 
-char *_strcpy(char *dest, const char *src)
+char *_strcpy(char *dest, char *src)
 {
-	int index = 0;
+	char *aux = dest;
 
-	while (src[index])
-	{
-		dest[index] = src[index];
-		index++;
-	}
-
-	return (dest);
+	while (*src)
+		*dest++ = *src++;
+	return (aux);
 }
