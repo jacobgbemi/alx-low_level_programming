1eb6d6cb109d32662a148119c34c96bcb5b6bace
Gbemi Jacob Adebayo <102582566+jacobgbemi@users.noreply.github.com>
2022-04-21 15:25:01 +0100

    reset to 98

---
+++ 0x05-pointers_arrays_strings/my_alternative_solutions/0-reset_to_98.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+void reset_to_98(int *n);
+
+int main(void)
+{
+     int n;
+    
+    n = 402;
+    printf("n=%d\n", n);
+    reset_to_98(&n);
+    printf("n=%d\n", n);
+    return (0);
+}
+
+void reset_to_98(int *n)
+{
+    *n = 98;
+}
