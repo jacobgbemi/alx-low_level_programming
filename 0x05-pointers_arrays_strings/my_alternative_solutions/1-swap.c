0a7f5db05675adcf9674f979525a97f56ea4cec6
Gbemi Jacob Adebayo <102582566+jacobgbemi@users.noreply.github.com>
2022-04-21 15:26:10 +0100

    swap

---
+++ 0x05-pointers_arrays_strings/my_alternative_solutions/1-swap.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void swap_int(int *a, int *b)
+{
+    int c = *a;
+    int d = *b;
+    *a = d;
+    *b = c;
+}
+int main(void)
+{
+    int a;
+    int b;
+    
+    a = 98;
+    b = 42;
+    
+    printf("a=%d, b=%d\n", a, b);
+    swap_int(&a, &b);
+    printf("a=%d, b=%d\n", a, b);
+    return (0);
+    
+}
