ecb1496c135db48cd8f2cf2f069c3f25d2a09172
Gbemi Jacob Adebayo <adebayogbemi90@gmail.com>
2022-04-24 09:57:04 +0000

    print a buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,6 +1,7 @@
 /*
  * Auth: Gbemi Jacob Adebayo
  */
+#include <stdio.h>
 
 /**
  * print_buffer - To print buffer
@@ -10,5 +11,41 @@
 
 void print_buffer(char *b, int size)
 {
+	int i, j, o;
 
+	o = 0;
+
+	if (size <= 0)
+	{
+		printf("\n");
+		return;
+	}
+	while (o < size)
+	{
+		j = size - o < 10 ? size - o : 10;
+		printf("%08x: ", o);
+		for (i = 0; i < 10; i++)
+		{
+			if (i < j)
+				printf("%02x", *(b + o + i));
+			else
+				printf("  ");
+			if (i % 2)
+			{
+				printf(" ");
+			}
+		}
+		for (i = 0; i < j; i++)
+		{
+			int c = *(b + o + i);
+
+			if (c < 32 || c > 132)
+			{
+				c = '.';
+			}
+			printf("%c", c);
+		}
+		printf("\n");
+		o += 10;
+	}
 }
