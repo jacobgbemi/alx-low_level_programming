c266919dfd10995357d6014b7e299e44b80f8549
Gbemi Jacob Adebayo <adebayogbemi90@gmail.com>
2022-04-25 20:20:10 +0000

    cat strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -16,16 +16,11 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i = 0, j = 0;
 
-	while (dest[i] != '\0)
-	{
-		i++;
-	}
-
-	while (src[j] != '\0' && j < n)
-		dest[i] = src[i];
-		i++;
+	while (dest[i++])
 		j++;
 
-	dest[i] = '\0';
-	return (temp);
+	for (i = 0; src[i] && i < n; i++)
+		dest[j++] = src[i];
+
+	return (dest);
 }
