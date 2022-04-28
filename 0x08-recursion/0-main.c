201e233702986967673e938e445070b3e67f0bb0
Gbemi Jacob Adebayo <adebayogbemi90@gmail.com>
2022-04-28 18:50:26 +0000

    first commit

---
+++ 0x08-recursion/0-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_puts_recursion("Puts with recursion");
+	return (0);
+}
