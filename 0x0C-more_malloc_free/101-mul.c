c6958bc4412fcff95a9693324e53957939b3ad0e
Gbemi Jacob Adebayo <adebayogbemi90@gmail.com>
2022-05-09 15:24:54 +0000

    infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,75 +1,99 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
+#include <stdio.h>
+#include "main.h"
 
 /**
- * is_num - iterate through each argv to test if it's a number
- * @argvv: a argv
- * Return: true only if entire string is a number, false if not
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
+ *
+ * Return: 0 if a non-digit is found, 1 otherwise
  */
-
-bool is_num(char *argvv)
+int is_digit(char *s)
 {
 	int i = 0;
 
-	for (i = 0; argvv[i]; i++)
+	while (s[i])
 	{
-		if (!(argvv[i] >= '0' && argvv[i] <= '9'))
+		if (s[i] < '0' || s[i] > '9')
 			return (0);
+		i++;
 	}
 	return (1);
 }
 
+/**
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
 
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
 
 /**
- * main - Check the code
- * @argc: number of arguments
- * @argv: arguments
- * Return: Nothing
+ * errors - handles errors for main
  */
-int main(int argc, char **argv)
+void errors(void)
 {
-	int num1, num2;
-	long int result;
-	int i = 1;
-	int j = 0, len = 0;
-	int *product = (int *)malloc(sizeof(size_t) * len);
+	printf("Error\n");
+	exit(98);
+}
 
-	while (product[j] != '\0')
-		len++, j++;
-	len++;
+/**
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	char *s1, *s2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
-	if (product == NULL)
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+		errors();
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	if (!result)
+		return (1);
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
-		exit(98);
-	}
-
-	if (argc != 3)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-	else
-	{
-		while (i < argc)
+		digit1 = s1[len1] - '0';
+		carry = 0;
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
-			if (is_num(argv[i]))
-			{
-			num1 = atoi(argv[1]);
-			num2 = atoi(argv[2]);
-			result = num1 * num2;
-			*product = result;
-			}
-			else
-			{
-				printf("Error\n");
-				exit(98);
-			}
-			i++;
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
 		}
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
 	}
-
-	printf("%d\n", *product);
+	for (i = 0; i < len - 1; i++)
+	{
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
+	}
+	if (!a)
+		_putchar('0');
+	_putchar('\n');
+	free(result);
 	return (0);
 }
