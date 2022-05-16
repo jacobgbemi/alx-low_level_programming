354a1b35792ae2356296478994984eb70baee2e4
Gbemi Jacob Adebayo <102582566+jacobgbemi@users.noreply.github.com>
2022-05-16 17:19:54 +0100

    updated

--- printf/main.c
+++ printf/main.c
@@ -3,8 +3,20 @@
 int main(void)
 {
 	myprintf("Hello world from C!\n");
-	myprintf("Formatted %% %c\r\n", 'a');
-	myprintf("Formatted %d %i %p %x %o %hd %hi %hu %hd\n", 1234, -5678, 0xdead, 0xbeef, 012345, (short)27, (short)-42, (unsigned char)20, (signed char)10);
-	/* myprintf(" %li %lx %ld %llx\r\n", (long int)-100001, ((long)size_t)0xful, 10200, (long int)0xdull); */
+	myprintf("Formatted %% %c\n", 'a');
+	printf("Formatted %% %c %s\n", 'string');
+	myprintf("Formatted %d \n", 1234);
+	myprintf("Formatted %i \n", -5678);
+	myprintf("Formatted %p \n", 0xdead);
+	myprintf("Formatted %x \n", 0xbeef);
+	myprintf("Formatted %o \n", 012345);
+	myprintf("Formatted %hd \n", (short)27);
+	myprintf("Formatted %hi \n", (short)-42);
+	myprintf("Formatted %hu \n", (unsigned char)20);
+	myprintf("Formatted %hd\n", (signed char)10);
+	printf(" %li \n", -1000000001);
+	printf(" %lx \n", 0xdeadbeeful);
+	printf(" %ld \n", 1020030040011);
+	printf(" %llx\n", 0xdeadbeeffeeddaedull);
 	return (0);
 }
