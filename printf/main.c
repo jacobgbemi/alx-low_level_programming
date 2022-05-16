505fee240c22ff12f5009a106f0b03c8b38810ba
Gbemi Jacob Adebayo <adebayogbemi90@gmail.com>
2022-05-16 16:06:52 +0000

    p

--- printf/main.c
+++ printf/main.c
@@ -2,9 +2,9 @@
 
 int main(void)
 {
-	puts("Hello world from C!\r\n");
-	myprintf("Formatted %% %c %s\r\n", 'a', 'string');
-	myprintf("Formatted %d %i %p %x %o %hd %hi %hu %hd\r\n", 1234, -5678, 0xdead, 0xbeef, 012345, (short)27, (short)-42, (unsigned char)20, (signed char)10);
-	printf(" %li %lx %ld %llx\r\n", -1000000001, 0xdeadbeeful, 1020030040011, 0xdeadbeeffeeddaedull);
+	myprintf("Hello world from C!\n");
+	myprintf("Formatted %% %c\r\n", 'a');
+	myprintf("Formatted %d %i %p %x %o %hd %hi %hu %hd\n", 1234, -5678, 0xdead, 0xbeef, 012345, (short)27, (short)-42, (unsigned char)20, (signed char)10);
+	/* myprintf(" %li %lx %ld %llx\r\n", (long int)-100001, ((long)size_t)0xful, 10200, (long int)0xdull); */
 	return (0);
 }
