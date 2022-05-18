7e7d48c12539384f9b4d75e79a9b838a088f72f6
Gbemi Jacob Adebayo <adebayogbemi90@gmail.com>
2022-05-18 11:19:16 +0000

    like

--- printf2/main.h
+++ printf2/main.h
@@ -10,6 +10,6 @@ int _putchar(char c);
 int _printf(const char * const format, ...);
 int print_char(va_list ap);
 int print_str(va_list ap);
-int (*select_func(char c)(va_list));
+int (*select_func(char c))(va_list);
 
 #endif
