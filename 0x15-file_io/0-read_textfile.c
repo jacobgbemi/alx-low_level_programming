63013ba124b7618bb4fc75925825e5fac0debf74
Gbemi Jacob Adebayo <adebayogbemi90@gmail.com>
2022-06-01 13:25:16 +0000

    correct betty in task 1

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -26,7 +26,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	o = open(filename, O_RDONLY);
 	if (o == -1)
 		return (0);
-	
+
 	r = read(o, buffer, letters);
 	w = write(STDOUT_FILENO, buffer, r);
 	if (r == -1 || w == -1)
