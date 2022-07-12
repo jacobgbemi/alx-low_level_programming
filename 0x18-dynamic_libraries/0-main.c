322050c97bf87215b8c3538e6a8cfd24fc88ff0e
Gbemi Jacob Adebayo <adebayogbemi90@gmail.com>
2022-07-12 09:12:24 +0000

    tasks

---
+++ 0x18-dynamic_libraries/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+	printf("%d\n", _strlen("Holberton"));
+	return (EXIT_SUCCESS);
+}
