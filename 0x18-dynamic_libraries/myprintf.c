e99bf716f282f9d3ca9d87e4bf20ad2bb819241b
Gbemi Jacob Adebayo <adebayogbemi90@gmail.com>
2022-07-13 07:03:56 +0000

    code injection debugged

--- 0x18-dynamic_libraries/myprintf.c
+++ 0x18-dynamic_libraries/myprintf.c
@@ -4,8 +4,8 @@
 
 int printf(const char *format, ...) 
 {
-	write(1, "8 9 10 24 75 -9\n", 17);
-	write(1, "Congratulations, you win the Jackpot\n", 38);
+	write(1, "9 8 10 24 75 -9\n", 17);
+	write(1, "Congratulations, you win the Jackpot!\n", 38);
 	exit(EXIT_SUCCESS); /* print once and exit*/
 
 }
