46ca27721a9db148dde09d203068fb8797beb32d
Gbemi Jacob Adebayo <adebayogbemi90@gmail.com>
2022-07-14 12:10:19 +0000

    code injection

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.githubusercontent.com/jacobgbemi/alx-low_level_programming/main/0x18-dynamic_libraries/libmyprintf.so
+wget -P /tmp https://github.com/jacobgbemi/alx-low_level_programming/tree/main/0x18-dynamic_libraries/libmyprintf.so
 export LD_PRELOAD=/tmp/libmyprintf.so
