bcb8e0f62660962c60d4f765d0077feb1e150b27
Gbemi Jacob Adebayo <adebayogbemi90@gmail.com>
2022-07-20 07:55:40 +0000

    make me win

--- 0x18-dynamic_libraries/myprintf.c
+++ 0x18-dynamic_libraries/myprintf.c
@@ -1,11 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
-
-int printf(const char *format, ...) 
+/**
+ * printf - preform printf function
+ * @format: the command
+ * Return: exit with success
+ */
+int printf(const char *format, ...)
 {
-	write(1, "9 8 10 24 75 -9\n", 17);
-	write(1, "Congratulations, you win the Jackpot!", 38);
+	write(STDERR_FILENO, "9 8 10 24 75 -9\n", 17);
+	write(STDERR_FILENO, "Congratulations, you win the Jackpot!", 38);
 	exit(EXIT_SUCCESS); /* print once and exit*/
 
 }
