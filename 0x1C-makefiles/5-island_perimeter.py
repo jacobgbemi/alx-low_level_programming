046d49da5ad5be4ca8d3aa6f245050398f264b70
Gbemi Jacob Adebayo <adebayogbemi90@gmail.com>
2022-09-14 12:02:43 +0000

    perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -9,12 +9,12 @@ def island_perimeter(grid):
     maxWidth = 0
     length = 0
     for i in range(len(grid)):
-       width = 0
-       for j in range(len(grid[0])):
-           if grid[i][j] == 1:
-               width += 1
-       if width:
-           length += 1
-       if width > maxWidth:
-           maxWidth = width
+        width = 0
+        for j in range(len(grid[0])):
+            if grid[i][j] == 1:
+                width += 1
+        if width:
+            length += 1
+        if width > maxWidth:
+            maxWidth = width
     return ((maxWidth + length) * 2)
