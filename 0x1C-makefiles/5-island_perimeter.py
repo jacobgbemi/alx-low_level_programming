c223af7f4b4e5a12c4d88b6036dce08a7a127cc1
Gbemi Jacob Adebayo <adebayogbemi90@gmail.com>
2022-09-14 14:16:50 +0000

    island perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,20 +1,38 @@
 #!/usr/bin/python3
+
 """
 Module Island Perimeter
 """
 
 
 def island_perimeter(grid):
-    """ Calculate perimeter of grid where "1" is found"""
-    maxWidth = 0
-    length = 0
-    for i in range(len(grid)):
-        width = 0
-        for j in range(len(grid[0])):
-            if grid[i][j] == 1:
-                width += 1
-        if width:
-            length += 1
-        if width > maxWidth:
-            maxWidth = width
-    return ((maxWidth + length) * 2)
+    """ Calculate perimeter of grid of an island
+     The grid represents water by 0 and land by 1.
+    Args:
+        grid (list): A list of list of integers representing an island.
+    Returns:
+        The perimeter of the island defined in grid.
+    """
+    p = 0
+    height = len(grid)
+    width = len(grid[0])
+    for lth in range(height):
+        for bth in range(width):
+            if (grid[lth][bth] == 1):
+                if (lth == 0):
+                    p += 1
+                if (grid[lth - 1][bth]) == 0:
+                    p += 1  # top surrounded by water
+                if (lth == (height - 1)):
+                    p += 1
+                if (grid[lth + 1][bth]) == 0:
+                    p += 1  # bottom surrounded by water
+                if (bth == 0):
+                    p += 1
+                if (grid[lth][bth - 1] == 0):
+                    p += 1  # left surrounded by water
+                if (bth == (width - 1)):
+                    p += 1
+                if (grid[lth][bth + 1]) == 0:
+                    p += 1  # right surrounded by water
+    return p
